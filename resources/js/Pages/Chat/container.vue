bbaa2825f56b205ea955d3bf0d45956af77fac75
gulfemnur <gulfemnur93@gmail.com>
2021-10-13 14:23:46 +0300

    update

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -2,17 +2,25 @@
     <app-layout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Chat
+                <chat-room-selection
+                    v-if="currentRoom.id"
+                    :rooms="chatRooms"
+                    :currentRoom="currentRoom"
+                    v-on:roomchange="setRoom($event)"
+                />
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <message-container :messages="messages" />
-                    <input-message 
+
+                    <message-container :messages="messages"/>
+                    <input-message
                         :room="currentRoom"
-                        v-on:messagesent="getMessages()"  />
+                        v-on:messagesent="getMessages()"
+                    />
+
                 </div>
             </div>
         </div>
@@ -20,51 +28,76 @@
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout.vue'
-    import InputMessage from './inputMessage.vue'
-    import MessageContainer from './messageContainer.vue'
-    
-    
-    export default ({
-        components: {
-            AppLayout,
-            MessageContainer,
-            InputMessage,            
-        },
-        data: function () {
-            return{
-                chatRooms: [],
-                currentRoom: [],
-                messages: []
+import AppLayout from '@/Layouts/AppLayout'
+import MessageContainer from "./messageContainer";
+import InputMessage from "./inputMessage";
+import ChatRoomSelection from "./chatRoomSelection";
+
+export default {
+    components: {
+        ChatRoomSelection,
+        AppLayout,
+        MessageContainer,
+        InputMessage
+    },
+    data: function () {
+        return {
+            chatRooms: [],
+            currentRoom: [],
+            messages: []
+        }
+    },
+
+    watch: {
+        currentRoom(val, oldVal) {
+            console.log(val, oldVal);
+            if (oldVal.id) {
+                this.disconnect(oldVal);
+            }
+            this.connect(val);
+        }
+    },
+    methods: {
+        connect(val) {
+            if (val.id) {
+                let vm = this;
+                this.getMessages();
+                window.Echo.private("chat." + val.id)
+                    .listen('NewChatMessage', e => {
+                        console.log(e);
+                        vm.getMessages();
+                    });
             }
         },
-        methods: {
-            getRooms () {
-                axios.get('/chat/rooms')
-                .then( response => {
+        disconnect(room) {
+            window.Echo.leave("chat." + room.id);
+        },
+        getRooms() {
+            axios.get('/chat/rooms')
+                .then(response => {
                     this.chatRooms = response.data;
-                    this.setRoom( response.data [0] );
+                    this.setRoom(response.data[0]);
                 })
                 .catch(error => {
-                    console.log( error );
+                    console.log(error);
                 })
-            },
-            setRoom ( room ) {
-                this.currentRoom = room;
-                this.getMessages();
-            },
-            getMessages(){
-                axios.get('/chat/room/' + this.currentRoom.id + '/messages')
-                .then( response => { 
+        },
+        setRoom(room) {
+            this.currentRoom = room;
+            this.getMessages();
+        },
+        getMessages() {
+            axios.get('/chat/room/' + this.currentRoom.id + '/messages')
+                .then(response => {
                     this.messages = response.data;
                 })
                 .catch(error => {
-                    console.log( error );
+                    console.log(error)
                 })
-            }
-        },
-        created() {
-            this.getRooms();
         }
-    })
+    },
+    created() {
+        this.getRooms();
+    }
+}
 </script>
